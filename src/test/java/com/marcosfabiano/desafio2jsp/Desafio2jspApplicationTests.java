fffff0b5730dc346fae1aafdecc66164dfd49f75
Marcos Fabiano <marcos.fabiano67@outlook.com>
2023-09-01 17:42:45 -0300

    Desafio pronto!

---
+++ src/test/java/com/marcosfabiano/desafio2jsp/Desafio2jspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosfabiano.desafio2jsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio2jspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
